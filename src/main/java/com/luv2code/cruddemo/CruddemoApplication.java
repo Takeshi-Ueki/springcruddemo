213f927548e1a96597fb930280ada96424213c59
Takeshi-Ueki <takeshi.c.0104@gmail.com>
2024-05-25 18:06:20 +0900

    StudentDAO追加 起動時にinsert処理追加

--- src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.StudentDAO;
+import com.luv2code.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,25 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
 		return runner -> {
-			System.out.println("Hello World");
+
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// studentオブジェクトの作成
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Paul", "Doe", "paul@lub2code.com");
+
+		// studentオブジェクトの保存
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		// 保存したstudentのidを表示
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 }
